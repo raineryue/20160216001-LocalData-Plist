8d9c168f3affe9a1c00f4e067e3cc3a2c9ea121c
raineryue <jinlei_yue@sina.com>
2016-02-16 14:45:40 +0800

    使用Plist存储数据
    使用Plist存储数据

--- 20160216001-LocalData-Plist/ViewController.m
+++ 20160216001-LocalData-Plist/ViewController.m
@@ -8,6 +8,10 @@
 
 #import "ViewController.h"
 
+// 获取Document的路径
+#define kDocumentPath NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0]
+#define kDataPlistName @"data.plist"
+
 @interface ViewController ()
 
 @end
@@ -17,6 +21,41 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+/**
+ *  存储数据点击事件
+ */
+- (IBAction)saveClickAction:(id)sender {
+    // 1.获取沙盒路径(第一种方法)
+//    NSString *sandBoxPath = NSHomeDirectory();
+//    
+//    NSString *documentsPath = [sandBoxPath stringByAppendingPathComponent:@"/Documents"];
+//    
+//    NSString *dataPlistPath = [documentsPath stringByAppendingPathComponent:kDataPlistName];
+//    
+//    NSLog(@"--------->%@", dataPlistPath);
+    
+    // 2.获取沙盒路径(第二种方法)，拼接plist的路径
+    NSString *dataPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0] stringByAppendingPathComponent:kDataPlistName];
+    
+    NSLog(@"--------->%@", dataPath);
+    
+    // 创建数据源数组
+    NSArray *dataArray = @[@1, @2, @"123"];
+    
+    // 将数组写入plist中
+    [dataArray writeToFile:dataPath atomically:YES];
+}
+
+/**
+ *  读取数据点击事件
+ */
+- (IBAction)readClickAction:(id)sender {
+    // 读取plist中缓存的数据
+    NSArray *dataArray = [NSArray arrayWithContentsOfFile:[kDocumentPath stringByAppendingPathComponent:kDataPlistName]];
+    
+    NSLog(@"%@", dataArray);
 }
 
 - (void)didReceiveMemoryWarning {
